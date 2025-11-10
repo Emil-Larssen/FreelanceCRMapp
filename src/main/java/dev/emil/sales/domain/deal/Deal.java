695baf853874d6c6ac15308147ae94f8e6c07621
Emil-larssen <emillarssne@gmail.com>
2025-11-10 11:17:05 +0100

    Deal state transitions implemented and added to test to verify their invariants. (OFFERED, NEGOTIATING, WON, LOST and CLOSED)

--- src/main/java/dev/emil/sales/domain/deal/Deal.java
+++ src/main/java/dev/emil/sales/domain/deal/Deal.java
@@ -1,7 +1,5 @@
 package dev.emil.sales.domain.deal;
-
 import dev.emil.sales.domain.prospect.Prospect;
-
 import java.util.UUID;
 
 public class Deal {
@@ -24,5 +22,29 @@ public class Deal {
     public UUID getDealProspectId() {
         return prospectId;
     }
+    public void sendOffer() {
+        if (state == DealState.DRAFT) {
+            this.state = DealState.OFFERED;
+        }
+    }
+    public void beginNegotiating() {
+        if (state == DealState.OFFERED) {
+            this.state = DealState.NEGOTIATING;
+        }
+    }
+    public void markDealWon() {
+        if (state == DealState.OFFERED || state == DealState.NEGOTIATING) {
+            this.state = DealState.WON;
+        }
+    }
+    public void markDealLost() {
+        if (state == DealState.OFFERED || state == DealState.NEGOTIATING) {
+            this.state = DealState.LOST;
+        }
+    }
+    public void closeDeal() {
+        this.state = DealState.CLOSED;
+    }
+
 
 }
