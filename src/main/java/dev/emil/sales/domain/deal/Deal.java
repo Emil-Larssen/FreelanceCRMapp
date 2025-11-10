821ec41d718635022d856205d791d3b10443f89b
Emil-larssen <emillarssne@gmail.com>
2025-11-10 10:23:48 +0100

    Prospect added with name & UUID. implemented in test to ensure that a new deal has prospect (verified by UUID) and is initialized in draft state.

--- src/main/java/dev/emil/sales/domain/deal/Deal.java
+++ src/main/java/dev/emil/sales/domain/deal/Deal.java
@@ -1,17 +1,28 @@
 package dev.emil.sales.domain.deal;
 
+import dev.emil.sales.domain.prospect.Prospect;
+
+import java.util.UUID;
+
 public class Deal {
     private DealState state;
+    private UUID prospectId;
 
-    private Deal(DealState state) {
+    private Deal(DealState state, UUID prospectId) {
         this.state = state;
+        this.prospectId = prospectId;
     }
 
-    public static Deal create() {
-        return new Deal(DealState.DRAFT);
+    public static Deal create(Prospect prospect) {
+        return new Deal(DealState.DRAFT, prospect.getUUID());
     }
 
     public DealState getState() {
         return state;
     }
+
+    public UUID getDealProspectId() {
+        return prospectId;
+    }
+
 }
--- src/test/java/dev/emil/sales/domain/deal/DealTests.java
+++ src/test/java/dev/emil/sales/domain/deal/DealTests.java
@@ -1,4 +1,5 @@
 package dev.emil.sales.domain.deal;
+import dev.emil.sales.domain.prospect.Prospect;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,9 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DealTests {
 
     @Test
-    void deal_created_with_draft_state() {
-        Deal deal = Deal.create();
-
+    // deal MUST initialize in DRAFT state, and MUST have a prospect.
+    void deal_created_with_draft_state_and_prospect() {
+        Prospect prospect = Prospect.create("Johnorg");
+        Deal deal = Deal.create(prospect);
         assertEquals(DealState.DRAFT, deal.getState());
+        assertNotNull(deal.getDealProspectId());
+
     }
+
 }
